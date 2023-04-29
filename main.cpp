9b72ad5a8e558399ad5fd5b847a56e90fde20d0a
Jaroslav Zhidkov <jaropolk1104@gmail.com>
2023-04-29 12:17:01 +0300

    Update main.cpp
    Функции всей игры и локаль, для возможности добавления русского языка

--- main.cpp
+++ main.cpp
@@ -1,1 +1,11 @@
+#include "game.h"
+#include "iostream"
+using namespace std;
+//функция всей игры
+int main() {
+    setlocale(LC_ALL, "Russian");
+    Game game;
+    game.run();
 
+    return 0;
+}
